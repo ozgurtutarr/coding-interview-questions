d696c19b9f59d6c772e898ce38e205445b478843
0xfree <ozgurtutarr@gmail.com>
2025-08-27 16:39:47 +0300

    part 1

---
+++ reversestring/index.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  //----------------- Solution 1
+  // return str.split('').reverse().join('');
+  //----------------- Solution 2
+  // let reversed = '';
+  // for (let char of str) {
+  //   reversed = char + reversed;
+  // }
+  // return reversed;
+  //----------------- Solution 3
+  // let reversed = '';
+  // for (let i = 0; i < str.length; i++) {
+  //   reversed = str[i] + reversed;
+  // }
+  // return reversed;
+  //----------------- Solution 4
+
+  return str.split('').reduce((reversed, char) => {
+    return char + reversed;
+  }, '');
+}
+
+module.exports = reverse;
