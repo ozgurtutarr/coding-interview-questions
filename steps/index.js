d13233d1812d6909361f47e69413f833cddac647
0xfree <ozgurtutarr@gmail.com>
2025-09-17 23:52:34 +0300

    Add new questions

--- steps/index.js
+++ steps/index.js
@@ -18,17 +18,17 @@
 //       '####'
 
 function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let stair = '';
-    for (let column = 0; column < n; column++) {
-      if (column <= row) {
-        stair += '#';
-      } else {
-        stair += ' ';
-      }
-    }
-    console.log(stair);
-  }
+  // for (let row = 0; row < n; row++) {
+  //   let stair = '';
+  //   for (let column = 0; column < n; column++) {
+  //     if (column <= row) {
+  //       stair += '#';
+  //     } else {
+  //       stair += ' ';
+  //     }
+  //   }
+  //   console.log(stair);
+  // }
 }
 
 module.exports = steps;
